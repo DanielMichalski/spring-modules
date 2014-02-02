2bb56bda7b106830af2eb2926fef4889b703b798
bakeraw2 <notarget1>
2014-02-02 23:03:26 +0100

    poprawki

--- L1/src/main/java/Runner.java
+++ L1/src/main/java/Runner.java
@@ -1,12 +1,16 @@
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
+import service.ITaskService;
 
 /**
  * Author: Daniel
  */
-public class Main {
+public class Runner {
     public static void main(String[] args) {
         ApplicationContext context =
                 new ClassPathXmlApplicationContext("context.xml");
+
+        ITaskService bean = (ITaskService) context.getBean("myService");
+        System.out.println(bean.getServiceId());
     }
 }
