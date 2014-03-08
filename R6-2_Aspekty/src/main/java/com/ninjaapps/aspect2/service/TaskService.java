cec151458eb81b4e01af9fc3bd88b928ad4fea05
bakeraw2 <notarget1>
2014-03-08 18:22:27 +0100

    poprawki

---
+++ R6-2_Aspekty/src/main/java/com/ninjaapps/aspect2/service/TaskService.java
@@ -0,0 +1,13 @@
+package com.ninjaapps.aspect2.service;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Author: Daniel
+ */
+@Component
+public class TaskService {
+    public void addTask(String taskName) {
+
+    }
+}
