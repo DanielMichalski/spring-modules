551c227e5ee48b9958c8360a734c972dfb7c4112
bakeraw2 <notarget1>
2014-03-16 18:20:15 +0100

    poprawki

--- R7-1-Warstwa-Web/src/main/java/com/springapp/mvc/controller/TaskController.java
+++ R7-1-Warstwa-Web/src/main/java/com/springapp/mvc/controller/TaskController.java
@@ -1,11 +1,16 @@
 package com.springapp.mvc.controller;
 
+import com.springapp.mvc.model.Task;
 import com.springapp.mvc.service.TaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
 
 /**
  * Author: Daniel
@@ -19,6 +24,18 @@ public class TaskController {
     @RequestMapping (value = "/all-task", method = RequestMethod.GET)
     public String allTasks(ModelMap model) {
         model.addAttribute("allTasks", taskService.findAllTasks());
+        model.addAttribute("task", new Task());
         return "all-tasks";
+    }
+
+    @RequestMapping (value = "/all-task", method = RequestMethod.POST)
+    public String saveTask(@Valid @ModelAttribute("task") Task task, BindingResult bindingResult, ModelMap modelMap) {
+        if (bindingResult.hasErrors()) {
+            return "all-tasks";
+        }
+
+        taskService.addTask(task);
+
+        return "redirect:/all-task";
     }
 }
